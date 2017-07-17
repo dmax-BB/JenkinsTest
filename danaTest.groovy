2c6989de8f290d402c0c7b22eb4e3275e4369546
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-17 13:36:38 +0000

    Starting over Mon Jul 17 13:36:38 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -7,21 +7,26 @@ node{
   //checkout scm: [$class: 'GitSCM', branches: [[name: "origin/master"]], userRemoteConfigs: [[credentialsId: "$env.DANA_TEST_CREDENTIALS", url: 'git@github.com:dmax-BB/JenkinsTest.git']], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'JenkinsTest4']]]
   //checkout scm: [$class: 'GitSCM', branches: [[name: "origin/master"]], userRemoteConfigs: [[credentialsId: "$env.DANA_TEST_CREDENTIALS", url: 'git@github.com:dmax-BB/JenkinsTest.git']], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'JenkinsTest5']]]
   //checkout scm: [$class: 'GitSCM', branches: [[name: "origin/master"]], userRemoteConfigs: [[credentialsId: "$env.DANA_TEST_CREDENTIALS", url: 'git@github.com:dmax-BB/JenkinsTest.git']], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'JenkinsTest6']]]
-
+ print "In node"
   stage('MyParallel') {
+   print "In MyParallel"
     [1,2].each {
       def a = it;
       //myBuilds[a] = { print "${a}\n" }
       myBuilds[a] = this.getTest(a)
-    }   
-  }    
+    }
+   print "Out MyParallel"
+  }
+ print "Out Note"
 }
 try{
+ print "In try"
   stage('RunParallel'){
+   print "In RunParallel"
   parallel myBuilds
   }
 } catch (Exception e){
-  throw e
+  print "I failed"
 }
 
 def getTest(String testFileName){
