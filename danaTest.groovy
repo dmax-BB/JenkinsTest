ee5c5c6f53485106aa4baf3c27998cbaabe3f02c
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-17 14:25:55 +0000

    Starting over Mon Jul 17 14:25:55 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -1,40 +1,40 @@
-def myBuilds = [:]
+// While you can't use Groovy's .collect or similar methods currently, you can
+// still transform a list into a set of actual build steps to be executed in
+// parallel.
 
-def Closure getTest(testFileName){
-  return {
-    node {
-      sh "ls -ltr"
-    }
-  }
+// Our initial list of strings we want to echo in parallel
+def stringsToEcho = ["a", "b", "c", "d"]
+
+// The map we'll store the parallel steps in before executing them.
+def stepsForParallel = [:]
+
+// The standard 'for (String s: stringsToEcho)' syntax also doesn't work, so we
+// need to use old school 'for (int i = 0...)' style for loops.
+for (int i = 0; i < stringsToEcho.size(); i++) {
+    // Get the actual string here.
+    def s = stringsToEcho.get(i)
+
+    // Transform that into a step and add the step to the map as the value, with
+    // a name for the parallel step as the key. Here, we'll just use something
+    // like "echoing (string)"
+    def stepName = "echoing ${s}"
+    
+    stepsForParallel[stepName] = transformIntoStep(s)
 }
 
-node{
-  print "In node"
+// Actually run the steps in parallel - parallel takes a map as an argument,
+// hence the above.
+parallel stepsForParallel
 
-  stage('MyParallel') {
-    print "In MyParallel"
-    [1,2].each {
-      def a = it;
-      def stepName = "echoing ${a}"
-      //myBuilds[a] = { print "${a}\n" }
-      myBuilds[stepName] = a
+// Take the string and echo it.
+def transformIntoStep(inputString) {
+    // We need to wrap what we return in a Groovy closure, or else it's invoked
+    // when this method is called, not when we pass it to parallel.
+    // To do this, you need to wrap the code below in { }, and either return
+    // that explicitly, or use { -> } syntax.
+    return {
+        node {
+            echo inputString
+        }
     }
-    print "Out MyParallel"
-  }
-
- print "Out node"
-
-  stage ('RunParallel') {
-    print "In RunParallel"
-    try{
-      print "In try"
-      myBuilds.failFast = true
-      parallel myBuilds
-      print "Out try"
-    } catch (Exception e){
-      print "I failed"
-      throw e
-    }
-    print "Out RunParallel"
-  }
 }
