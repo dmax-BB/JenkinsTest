ff676873c23c8ef9bfe520c79c88e22c30ab970b
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-17 13:58:00 +0000

    Starting over Mon Jul 17 13:58:00 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -5,22 +5,27 @@ def Closure getTest(String testFileName){
 }
 
 node{
- print "In node"
+  print "In node"
 
   stage('MyParallel') {
-   print "In MyParallel"
+    print "In MyParallel"
     [1,2].each {
       def a = it;
       //myBuilds[a] = { print "${a}\n" }
       myBuilds[a] = { getTest(a) }
     }
-   print "Out MyParallel"
+    print "Out MyParallel"
   }
 
- print "Out Note"
-}
-try{
- print "In try"
-} catch (Exception e){
-  print "I failed"
+ print "Out node"
+
+  stage ('RunParallel') {
+    print "In RunParallel"
+    try{
+      print "In try"
+    } catch (Exception e){
+      print "I failed"
+    }
+    print "Out RunParallel"
+  }
 }
