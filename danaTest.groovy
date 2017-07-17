27e371349d26705c3495267eb7061e9f7ab8e00d
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-17 14:35:26 +0000

    Starting over Mon Jul 17 14:35:26 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -1,12 +1,18 @@
 def stringsToEcho = ["a", "b", "c", "d"]
-
 def myBuilds = [:]
+def repoMap = [:]
+repoMap.put('rails','git@github.com:rails/rails.git')
+repoMap.put('tensorflow','git@github.com:tensorflow/tensorflow.git')
+repoMap.put('bootstrap','git@github.com:twbs/bootstrap.git')
+repoMap.put('freeCodeCamp','git@github.com:freeCodeCamp/freeCodeCamp.git')
+repoMap.put('react','git@github.com:facebook/react.git')
+repoMap.put('angular.js','git@github.com:angular/angular.js.git')
 
 node {
   stage('MyParallel') {
     for (int i = 0; i < stringsToEcho.size(); i++) {
       def s = stringsToEcho.get(i)
-      def stepName = "echoing ${s}"
+      def stepName = "Sync number [${i}]"
       myBuilds[stepName] = syncCode(s)
     }
   }
