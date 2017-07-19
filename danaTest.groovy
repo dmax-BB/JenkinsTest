a1e14aa8c883179955c895c32fc5369dc9dbda70
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-19 18:20:38 +0000

    Starting over Wed Jul 19 18:20:38 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -18,12 +18,12 @@ node {
 
   stage('clonePreparation') {
   parallel (
-    clone1: { cloneCode('origin/master','git@github.com:rails/rails.git','rails' },
-    clone2: { cloneCode('origin/master','git@github.com:tensorflow/tensorflow.git','tensorflow' },
-    clone3: { cloneCode('origin/master','git@github.com:twbs/bootstrap.git','bootstrap' },
-    clone4: { cloneCode('origin/master','git@github.com:freeCodeCamp/freeCodeCamp.git','freeCodeCamp' },
-    clone5: { cloneCode('origin/master','git@github.com:facebook/react.git','react' },
-    clone6: { cloneCode('origin/master','git@github.com:angular/angular.js.git','angular.js' }
+    clone1: { cloneCode('origin/master','git@github.com:rails/rails.git','rails') },
+    clone2: { cloneCode('origin/master','git@github.com:tensorflow/tensorflow.git','tensorflow') },
+    clone3: { cloneCode('origin/master','git@github.com:twbs/bootstrap.git','bootstrap') },
+    clone4: { cloneCode('origin/master','git@github.com:freeCodeCamp/freeCodeCamp.git','freeCodeCamp') },
+    clone5: { cloneCode('origin/master','git@github.com:facebook/react.git','react') },
+    clone6: { cloneCode('origin/master','git@github.com:angular/angular.js.git','angular.js') }
   )
   }
 
