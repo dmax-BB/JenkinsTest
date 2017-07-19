0c00ac312ae7e6380e5336c647509c5201094cd5
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-19 18:34:54 +0000

    Starting over Wed Jul 19 18:34:54 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -11,30 +11,22 @@ repositoryMapping.react='git@github.com:facebook/react.git'
 repositoryMapping.put('angular.js','git@github.com:angular/angular.js.git')
 
 node {
-  // 1st stage to set up the clone step
-  //stage('clonePreparation') {
-    //processReposToClone(repositoryMapping,reposToClone)
-  //}
-  stage('CloneRepos') {
-    parallel (
-      clone1: cloneCode('origin/master','git@github.com:rails/rails.git','rails'),
-      clone2: cloneCode('origin/master','git@github.com:tensorflow/tensorflow.git','tensorflow'),
-      clone3: cloneCode('origin/master','git@github.com:twbs/bootstrap.git','bootstrap'),
-      clone4: cloneCode('origin/master','git@github.com:freeCodeCamp/freeCodeCamp.git','freeCodeCamp'),
-      clone5: cloneCode('origin/master','git@github.com:facebook/react.git','react'),
-      clone6: cloneCode('origin/master','git@github.com:angular/angular.js.git','angular.js')
-    )
-  }
-
+  // Run cloning
   try{
-    stage('RunCloning') {
-      // Execute the cloning in parallel
-      //parallel reposToClone
-      print "$env.WORKSPACE"
+    stage('CloneRepos') {
+      parallel (
+        clone1: cloneCode('origin/master','git@github.com:rails/rails.git','rails'),
+        clone2: cloneCode('origin/master','git@github.com:tensorflow/tensorflow.git','tensorflow'),
+        clone3: cloneCode('origin/master','git@github.com:twbs/bootstrap.git','bootstrap'),
+        clone4: cloneCode('origin/master','git@github.com:freeCodeCamp/freeCodeCamp.git','freeCodeCamp'),
+        clone5: cloneCode('origin/master','git@github.com:facebook/react.git','react'),
+        clone6: cloneCode('origin/master','git@github.com:angular/angular.js.git','angular.js')
+      )
     }
   } catch (Exception e){
     throw e
   }
+
 }
 
 // Method to run the actual clone of the repository
