6f3d69b73fb5d540a91501f7c6489f637a830721
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-31 18:26:21 +0000

    Change to pass nodename to method Mon Jul 31 18:26:21 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -6,12 +6,12 @@ node ('DanaNode') {
   try{
     stage('CloneRepos') {
       parallel (
-        "rails clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/rails/rails.git','rails',"${env.DANA_TEST_CREDENTIALS}","${env.RAILS_BRANCH}"),
+        "rails clone": new SourceClone().cloneSourceCode('DanaNodeBAD','ssh://git@github.com/rails/rails.git','rails',"${env.DANA_TEST_CREDENTIALS}","${env.RAILS_BRANCH}"),
         "tensorflow clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/tensorflow/tensorflow.git','tensorflow',"${env.DANA_TEST_CREDENTIALS}","${env.TENSORFLOW_BRANCH}"),
-        "bootstrap clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/twbs/bootstrap.git','bootstrap',"${env.DANA_TEST_CREDENTIALS}","${env.BOOTSTRAP_BRANCH}"),
+        "bootstrap clone": new SourceClone().cloneSourceCode('DanaNodeBAD','ssh://git@github.com/twbs/bootstrap.git','bootstrap',"${env.DANA_TEST_CREDENTIALS}","${env.BOOTSTRAP_BRANCH}"),
         "freeCodeCamp clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/freeCodeCamp/freeCodeCamp.git','freeCodeCamp',"${env.DANA_TEST_CREDENTIALS}","${env.FREECODECAMP_BRANCH}"),
         "react clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/facebook/react.git','react',"${env.DANA_TEST_CREDENTIALS}","${env.REACT_BRANCH}"),
-        "angular.js clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/angular/angular.js.git','angular.js',"${env.DANA_TEST_CREDENTIALS}","${env.ANGULARJS_BRANCH}")
+        "angular.js clone": new SourceClone().cloneSourceCode('DanaNodeBAD','ssh://git@github.com/angular/angular.js.git','angular.js',"${env.DANA_TEST_CREDENTIALS}","${env.ANGULARJS_BRANCH}")
       )
     }
   } catch (Exception e){
