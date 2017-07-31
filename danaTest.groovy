39655f484fcaf308ba39f85503bc3c4ff9d91d1e
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-31 16:48:35 +0000

    Change to pass nodename to method Mon Jul 31 16:48:35 UTC 2017

--- danaTest.groovy
+++ danaTest.groovy
@@ -1,17 +1,17 @@
 @Library('jenkins-pipeline-libraries')
 import com.danasoftware.jenkins.pipeline.libraries.SourceClone
 
-node {
+node ('DanaNode') {
   // Run cloning
   try{
     stage('CloneRepos') {
       parallel (
-        "rails clone": new SourceClone().cloneSourceCode('ssh://git@github.com/rails/rails.git','rails',"${env.DANA_TEST_CREDENTIALS}","${env.RAILS_BRANCH}"),
-        "tensorflow clone": new SourceClone().cloneSourceCode('ssh://git@github.com/tensorflow/tensorflow.git','tensorflow',"${env.DANA_TEST_CREDENTIALS}","${env.TENSORFLOW_BRANCH}"),
-        "bootstrap clone": new SourceClone().cloneSourceCode('ssh://git@github.com/twbs/bootstrap.git','bootstrap',"${env.DANA_TEST_CREDENTIALS}","${env.BOOTSTRAP_BRANCH}"),
-        "freeCodeCamp clone": new SourceClone().cloneSourceCode('ssh://git@github.com/freeCodeCamp/freeCodeCamp.git','freeCodeCamp',"${env.DANA_TEST_CREDENTIALS}","${env.FREECODECAMP_BRANCH}"),
-        "react clone": new SourceClone().cloneSourceCode('ssh://git@github.com/facebook/react.git','react',"${env.DANA_TEST_CREDENTIALS}","${env.REACT_BRANCH}"),
-        "angular.js clone": new SourceClone().cloneSourceCode('ssh://git@github.com/angular/angular.js.git','angular.js',"${env.DANA_TEST_CREDENTIALS}","${env.ANGULARJS_BRANCH}")
+        "rails clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/rails/rails.git','rails',"${env.DANA_TEST_CREDENTIALS}","${env.RAILS_BRANCH}"),
+        "tensorflow clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/tensorflow/tensorflow.git','tensorflow',"${env.DANA_TEST_CREDENTIALS}","${env.TENSORFLOW_BRANCH}"),
+        "bootstrap clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/twbs/bootstrap.git','bootstrap',"${env.DANA_TEST_CREDENTIALS}","${env.BOOTSTRAP_BRANCH}"),
+        "freeCodeCamp clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/freeCodeCamp/freeCodeCamp.git','freeCodeCamp',"${env.DANA_TEST_CREDENTIALS}","${env.FREECODECAMP_BRANCH}"),
+        "react clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/facebook/react.git','react',"${env.DANA_TEST_CREDENTIALS}","${env.REACT_BRANCH}"),
+        "angular.js clone": new SourceClone().cloneSourceCode('DanaNode','ssh://git@github.com/angular/angular.js.git','angular.js',"${env.DANA_TEST_CREDENTIALS}","${env.ANGULARJS_BRANCH}")
       )
     }
   } catch (Exception e){
